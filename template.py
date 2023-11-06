309c1d162d52813b737778d5c746ace5e4b53ec8
stonneau <stonneau@users.noreply.github.com>
2023-11-06 13:11:48 +0000

    t2 draft

--- template.py
+++ template.py
@@ -2,14 +2,13 @@ from motor_control.AROMotorControl import AROMotorControl
 
 motor_control = AROMotorControl()
 
-
-if __name__ == "__main__":
-
+def run_try_catch(func, *args, **kwargs):
+    '''helper function to encaspsulate try / catch template'''
     try:
         # Please run your code in a try-catch block so that when you terminate 
         # your program (using ctrl+c or otherwise) or an error occurs, the motors stop.
         # Otherwise the motors will keep executing the last command applied to them.
-        pass
+        func(*args, **kwargs)
     except KeyboardInterrupt:
         print("KeyboardInterrupt received, stopping motors...")
     except Exception as e:
@@ -18,3 +17,12 @@ if __name__ == "__main__":
         motor_control.applyCurrentToMotor(1, 0)
         motor_control.applyCurrentToMotor(2, 0)
         print("motors stopped!")
+
+
+
+if __name__ == "__main__":
+
+    def func():
+        pass     
+    
+    run_try_catch(func)
