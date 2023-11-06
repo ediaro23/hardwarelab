c3ceb432cf9e023ac80fcb2feda29f850afcf214
Abdul <arehmannasir@outlook.com>
2023-11-06 17:08:16 +0000

    lab 2 instructions ready

--- template.py
+++ template.py
@@ -18,11 +18,18 @@ def run_try_catch(func, *args, **kwargs):
         motor_control.applyCurrentToMotor(2, 0)
         print("motors stopped!")
 
-
-
 if __name__ == "__main__":
 
-    def func():
-        pass     
-    
-    run_try_catch(func)
+    try:
+        # Please run your code in a try-catch block so that when you terminate 
+        # your program (using ctrl+c or otherwise) or an error occurs, the motors stop.
+        # Otherwise the motors will keep executing the last command applied to them.
+        pass
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt received, stopping motors...")
+    except Exception as e:
+        print(f"an error occurred: {e}")
+    finally:
+        motor_control.applyCurrentToMotor(1, 0)
+        motor_control.applyCurrentToMotor(2, 0)
+        print("motors stopped!")
